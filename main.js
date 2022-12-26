c9542852060db04ec2493f8cacbcc21e52840583
JASWANT SINGH <81209296+Sjaswant55@users.noreply.github.com>
2022-12-27 01:19:04 +0530

    Added All  Files to our new project

---
+++ main.js
@@ -0,0 +1,24 @@
+let n1=Math.random();
+n1=n1*6;
+let n11=Math.floor(n1)+1;
+
+let n2=Math.random();
+n2=n2*6;
+let n22=Math.floor(n2)+1;
+
+
+let randomNumber1="images/dice"+ n11 + ".png";
+let randomNumber2="images/dice"+ n22 + ".png";
+
+ 
+
+document.querySelectorAll("img")[0].setAttribute("src",randomNumber1);
+document.querySelectorAll("img")[1].setAttribute("src",randomNumber2);
+
+if(n11>n22)
+ alert("player "+1+" win");
+else if(n11<n22)
+ alert("player "+2+" win");
+else 
+  alert("tied");
+ 
